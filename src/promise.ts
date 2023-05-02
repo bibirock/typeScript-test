f1e1921f562490b6bb2b4d6a1ffe7a3991294f53
Ben.Chen <ben.chen@keyxentic.com>
2023-05-02 11:29:11 +0800

    feat: 移除多餘程式碼

--- src/promise.ts
+++
@@ -1,9 +0,0 @@
-//定義promise物件回傳的型別，如果沒有定義的話，將會無法自動堆導
-
-function promise(): Promise<number> {
-    return new Promise<number>((resolve) => {
-        resolve(1);
-    });
-}
-
-promise().then((res) => console.log(res));
